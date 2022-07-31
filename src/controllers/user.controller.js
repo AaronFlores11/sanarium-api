fdbf918730388c1c7d1d5c78b7f97fc0b81bf3ef
Aaron Flores <aaron.sales.flores@gmail.com>
2022-07-30 22:47:26 -0700

    Ultimos cambios de token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,11 +5,9 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 module.exports = {
+  /*Crear un usuario */
   createUser: async function (req, res) {
     const { _id, name, last_name, email, password } = req.body;
-    console.log("Llega la informacion");
-    console.log(_id, name, last_name, email, password);
-    console.log(req.body);
 
     //Validacion si existe ya el email dentro de la bd
     const isEmailExist = await User.findOne({ email });
@@ -34,7 +32,14 @@ module.exports = {
     try {
       const savedUser = await user.save();
 
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        process.env.SECRET
+      );
       return res.status(200).json({
+        data: { token },
         _id: savedUser._id,
         message: "Successful registration",
       });
@@ -42,29 +47,52 @@ module.exports = {
       res.status(400).json({ error });
     }
   },
-
+  /*Login para usuario */
   loginUser: async function (req, res) {
     // Validaciond e existencia
     const user = await User.findOne({ email: req.body.email });
 
-    if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
+    if (!user) return res.status(400).json({ messageError: "User not found" });
 
     // Validacion de password en la base de datos
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
     );
-
     if (!validPassword)
-      return res.status(400).json({ error: "Constraseña invalida" });
+      return res.status(400).json({ messageError: "Invalid password" });
 
-    return res.status(200).json({
-      message: "bienvenido",
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.SECRET
+    );
+
+    return res.status(200).header("auth-token", token).json({
+      data: { token },
+      id: user._id,
+      message: "Bienvenido",
     });
   },
-
+  /*Traer un solo usuario*/
+  getSingleUser: async function (req, res) {
+    const { id } = req.params;
+    console.log(id);
+    const user = await User.findById({ _id: id });
+    try {
+      return res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        last_name: user.last_name,
+        email: user.email,
+      });
+    } catch (error) {
+      return res.status(400).json({ messageError: "User not found" });
+    }
+  },
   /*Solo para pruebas, eliminar cuando terminen pruebas */
-  getUser: async function (req, res) {
+  getUsers: async function (req, res) {
     const { username, email, password, roles } = req.body;
 
     //Validacion si existe ya el email dentro de la bd
